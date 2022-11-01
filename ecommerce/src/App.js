6b4f7ddff3863753e0297ffec35c3e5b3af39614
mo2men-5aled <momenk208@gmail.com>
2022-11-01 02:34:57 +0200

    update the app file

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import CARD from "./components/cards";
-import SIDEBAR from "./components/sidebar";
 
 const App = () => {
   return (
     <div>
-      <SIDEBAR />
       <CARD />
     </div>
   );
