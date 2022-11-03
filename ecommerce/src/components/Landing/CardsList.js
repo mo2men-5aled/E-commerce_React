a4e8ddc4844539662221b1d456167fc8c024edc3
mo2men-5aled <momenk208@gmail.com>
2022-11-03 22:17:29 +0200

    List of Cards

---
+++ ecommerce/src/components/Landing/CardsList.js
@@ -0,0 +1,30 @@
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import Data from "../../Data/db.json";
+const Cards = () => {
+  const items = Data.items;
+  return (
+    <Container>
+      <Row xs={2} md={3} lg={4} className="g-5">
+        {items.map((item) => {
+          return (
+            <Col key={item.id}>
+              <Card>
+                <Card.Img variant="top" src={item.image} />
+                <Card.Body>
+                  <Card.Title>{item.name}</Card.Title>
+                  <Card.Text>{item.description}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
+};
+
+export default Cards;
--- ecommerce/src/components/Routes/Landing.js
+++ ecommerce/src/components/Routes/Landing.js
@@ -1,9 +1,16 @@
 import Carousal from "../Landing/HeadImages";
-
+import Cards from "../Landing/CardsList";
 const Landing = () => {
   return (
     <div>
       <Carousal />
+      <div
+        className="container"
+        style={{ textAlign: "center", padding: "30px" }}
+      >
+        <h1>Latest Products</h1>
+      </div>
+      <Cards />
     </div>
   );
 };
