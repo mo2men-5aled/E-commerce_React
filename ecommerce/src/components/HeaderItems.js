2c3452b3c8c8f00facc283ab891da38546ce53ac
mo2men-5aled <momenk208@gmail.com>
2022-11-04 15:50:41 +0200

    update header tabs

--- ecommerce/src/components/HeaderItems.js
+++ ecommerce/src/components/HeaderItems.js
@@ -1,5 +1,4 @@
 import Item from "./Item";
-
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
@@ -19,8 +18,9 @@ const HeaderItems = () => {
       </Offcanvas.Header>
       <Offcanvas.Body>
         <Nav className="justify-content-end flex-grow-1 pe-3">
-          <Item txt="Home" />
-          <Item txt="Contacts" />
+          <Item path="/" txt="Home" />
+
+          <Item path="/list" txt="Our Products" />
           <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown-expand-sm">
             <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
--- ecommerce/src/components/Item.js
+++ ecommerce/src/components/Item.js
@@ -1,7 +1,7 @@
 import Nav from "react-bootstrap/Nav";
 
 const Item = (props) => {
-  return <Nav.Link href="#action1">{props.txt}</Nav.Link>;
+  return <Nav.Link href={props.path}>{props.txt}</Nav.Link>;
 };
 
 export default Item;
