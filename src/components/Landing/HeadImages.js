73b23ab7c1df2a3d1c3dce914e51b66517f80025
mo2men-5aled <momenk208@gmail.com>
2022-11-04 20:10:02 +0200

    fix carusal size

--- src/components/Landing/HeadImages.js
+++ src/components/Landing/HeadImages.js
@@ -5,25 +5,13 @@ const Carousal = () => {
     <div style={{ width: "100%", height: "100%" }}>
       <Carousel>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://picsum.photos/id/314/1000/300"
-            alt="First slide"
-          />
+          <img src="https://picsum.photos/id/314/1000/300" alt="First slide" />
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://picsum.photos/id/313/1000/300"
-            alt="Second slide"
-          />
+          <img src="https://picsum.photos/id/313/1000/300" alt="Second slide" />
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://picsum.photos/id/311/1000/300"
-            alt="Third slide"
-          />
+          <img src="https://picsum.photos/id/311/1000/300" alt="Third slide" />
         </Carousel.Item>
       </Carousel>
     </div>
